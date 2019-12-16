c1794e459d9f98659c821bc1a594e5ef05457cf8
Julien SULPIS <julien.sulpis@gmail.com>
2019-12-16 23:44:27 +0100

    fix Date in fetchRepos

--- infrastructure/__tests__/fetchRepos.spec.ts
+++ infrastructure/__tests__/fetchRepos.spec.ts
@@ -1,4 +1,5 @@
 import httpGet from "lib/httpGet";
+import Repository from "models/Repository";
 import fetchRepos from "../fetchRepos";
 
 jest.mock("lib/httpGet");
@@ -14,10 +15,10 @@ describe("Repo api", () => {
     const repos = await fetchRepos("jsulpis");
 
     // Then
-    const expectedReposList = [
+    const expectedReposList: Repository[] = [
       {
         archived: false,
-        creationDate: "2018-06-01T15:10:08Z",
+        creationDate: new Date("2018-06-01T15:10:08Z"),
         description: "A set of common Android components.",
         forked: false,
         forks: 0,
@@ -26,12 +27,12 @@ describe("Repo api", () => {
         name: "android-modules",
         size: 279,
         stars: 0,
-        updateDate: "2018-10-25T12:18:19Z",
+        updateDate: new Date("2018-10-25T12:18:19Z"),
         url: "https://github.com/jsulpis/android-modules"
       },
       {
         archived: false,
-        creationDate: "2018-01-25T21:00:53Z",
+        creationDate: new Date("2018-01-25T21:00:53Z"),
         description: "My repository of add-ons for Blender.",
         forked: false,
         forks: 1,
@@ -40,7 +41,7 @@ describe("Repo api", () => {
         name: "blender-addons",
         size: 1076,
         stars: 2,
-        updateDate: "2019-06-10T15:26:14Z",
+        updateDate: new Date("2019-06-10T15:26:14Z"),
         url: "https://github.com/jsulpis/blender-addons"
       }
     ];
