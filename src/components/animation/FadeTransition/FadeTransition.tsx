df943a470ae8b9bed36bbd38ddda063eff176b4b
Julien Sulpis <julien.sulpis@gmail.com>
2022-01-27 22:09:08 +0100

    fix: layout on small screens

--- src/components/animation/FadeTransition/FadeTransition.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-import styles from "./FadeTransition.module.scss";
-
-export const FadeTransition: FC = ({ children }) => (
-  <div className={styles.fade}>{children}</div>
-);
