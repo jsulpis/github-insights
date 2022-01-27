53f8c696a66ccf801cce49eeb234e9f185982a5d
Julien Sulpis <julien.sulpis@gmail.com>
2022-01-27 13:39:38 +0100

    feat: update the profile page with modern React syntax

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -12,122 +12,97 @@ import { ContributionsPerRepo } from "models/Contributions";
 import { RepositoryContributedTo, RepositoryOwned } from "models/Repository";
 import TimelineData from "models/TimelineData";
 import User from "models/User";
-import { withRouter } from "next/router";
-import React from "react";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
 
 interface UserPageState {
-  user: User;
-  reposOwned: RepositoryOwned[];
-  reposContributedTo: RepositoryContributedTo[];
-  timelineData: TimelineData;
-  contributionsPerRepo: ContributionsPerRepo[];
+  user?: User;
+  reposOwned?: RepositoryOwned[];
+  reposContributedTo?: RepositoryContributedTo[];
+  timelineData?: TimelineData;
+  contributionsPerRepo?: ContributionsPerRepo[];
 }
 
-const defaultState: UserPageState = {
-  user: null,
-  reposOwned: null,
-  reposContributedTo: null,
-  timelineData: {
-    totalContributions: 0,
-    contributionsPerMonth: []
-  },
-  contributionsPerRepo: []
+const UserPage: FC = () => {
+  const router = useRouter();
+  const [state, setState] = useState<UserPageState>({});
+  const username = router.query.username;
+
+  useEffect(() => {
+    if (!username) {
+      return;
+    }
+    Promise.all([
+      apiGet<User>("/" + username),
+      apiGet<RepositoryOwned[]>("/" + username + "/repos-owned"),
+      apiGet<RepositoryContributedTo[]>("/" + username + "/repos-contributed"),
+      apiGet<TimelineData>("/" + username + "/timeline"),
+      apiGet<ContributionsPerRepo[]>("/" + username + "/contributions")
+    ])
+      .then(([user, reposOwned, reposContributedTo, timelineData, contributionsPerRepo]) => {
+        setState({
+          user,
+          reposOwned,
+          reposContributedTo,
+          timelineData,
+          contributionsPerRepo
+        });
+      })
+      .catch(e => {
+        if (e.status === 404) {
+          router.push("/404");
+        } else {
+          router.push("/error");
+        }
+      });
+  }, [router, username]);
+
+  const { user, reposOwned, reposContributedTo, timelineData, contributionsPerRepo } = state;
+
+  const backgroundPictureSeed = user ? user.name + new Date().getMinutes().toString() : null;
+  const isDataPresent = !!user && !!reposOwned;
+
+  function onNewUsernameSubmitted(newUsername) {
+    if (newUsername !== username) {
+      setState({});
+      router.push("/[username]", "/" + newUsername);
+    }
+  }
+
+  return (
+    <Page>
+      {isDataPresent ? (
+        <FadeTransition>
+          <SearchForm searchUser={onNewUsernameSubmitted} />
+          <UserProfile
+            user={user}
+            repos={reposOwned}
+            backgroundPictureSeed={backgroundPictureSeed}
+          />
+          <ContributionsChart
+            timelineData={timelineData}
+            contributionsPerRepo={contributionsPerRepo}
+          />
+          <LanguagesCharts repos={reposOwned} />
+          <RepositoriesCharts repos={reposContributedTo} />
+          <Footer />
+        </FadeTransition>
+      ) : (
+        <>
+          <p className="h5">Hold on a second, I'm fetching your data...</p>
+          <Spinner />
+        </>
+      )}
+      {backgroundPictureSeed && (
+        // Prefetch the background image
+        <img
+          alt="background"
+          style={{ display: "none" }}
+          src={`https://picsum.photos/seed/${backgroundPictureSeed}/800/130`}
+        />
+      )}
+    </Page>
+  );
 };
 
-class UserPage extends React.Component<any, UserPageState> {
-  constructor(args) {
-    super(args);
-    this.state = defaultState;
-  }
-
-  public render() {
-    const { user, reposOwned, reposContributedTo, timelineData } = this.state;
-
-    const backgroundPictureSeed = user
-      ? user.name + new Date().getMinutes().toString()
-      : null;
-    const contributionsPerRepo = this.state.contributionsPerRepo;
-    const isDataPresent = !!user && !!reposOwned;
-
-    return (
-      <Page>
-        {isDataPresent ? (
-          <FadeTransition>
-            <SearchForm
-              searchUser={username => {
-                this.setState(defaultState);
-                this.props.router.push("/[username]", "/" + username);
-              }}
-            />
-            <UserProfile
-              user={user}
-              repos={reposOwned}
-              backgroundPictureSeed={backgroundPictureSeed}
-            />
-            <ContributionsChart
-              timelineData={timelineData}
-              contributionsPerRepo={contributionsPerRepo}
-            />
-            <LanguagesCharts repos={reposOwned} />
-            <RepositoriesCharts repos={reposContributedTo} />
-            <Footer />
-          </FadeTransition>
-        ) : (
-          <>
-            <p className="h5">Hold on a second, I'm fetching your data...</p>
-            <Spinner />
-          </>
-        )}
-        {backgroundPictureSeed && (
-          // Prefetch the background image
-          <img
-            alt="background"
-            style={{ display: "none" }}
-            src={`https://picsum.photos/seed/${backgroundPictureSeed}/800/130`}
-          />
-        )}
-      </Page>
-    );
-  }
-
-  public componentDidMount() {
-    const username = this.props.router.query.username;
-    if (!!username) {
-      this.fetchAllData(username);
-    }
-  }
-
-  public componentDidUpdate(prevProps) {
-    const username = this.props.router.query.username;
-    // verify props have changed to avoid an infinite loop
-    if (!!username && username !== prevProps.router.query.username) {
-      this.fetchAllData(username);
-    }
-  }
-
-  private fetchAllData(username) {
-    Promise.all([
-      apiGet<User>("/" + username).then(user => this.setState({ user })),
-      apiGet<RepositoryOwned[]>("/" + username + "/repos-owned").then(reposOwned =>
-        this.setState({ reposOwned })
-      ),
-      apiGet<RepositoryContributedTo[]>("/" + username + "/repos-contributed").then(
-        reposContributedTo => this.setState({ reposContributedTo })
-      ),
-      apiGet<TimelineData>("/" + username + "/timeline").then(timelineData =>
-        this.setState({ timelineData })
-      ),
-      apiGet<ContributionsPerRepo[]>("/" + username + "/contributions").then(
-        contributionsPerRepo => this.setState({ contributionsPerRepo })
-      )
-    ]).catch(e => {
-      if (e.status === 404) {
-        this.props.router.push("/404");
-      } else {
-        this.props.router.push("/error");
-      }
-    });
-  }
-}
-
-export default withRouter(UserPage);
+export default UserPage;
