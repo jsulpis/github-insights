d832f464390209cfeeac670b804086a701c935ad
Julien Sulpis <julien.sulpis@gmail.com>
2022-01-28 19:26:54 +0100

    feat: update SEO meta tags and og:image

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="language" content="en" />
+          <meta httpEquiv="content-language" content="en-us" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=optional"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
