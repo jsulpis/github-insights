a5b0ad4b70ff95889b33ab481c265d6c1a67c98b
Julien Sulpis <julien.sulpis@gmail.com>
2022-01-28 19:26:01 +0100

    chore: update env variables and remove unnecessary configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
 const webpack = require("webpack");
 const path = require("path");
-const withFonts = require("next-fonts");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
@@ -15,9 +13,8 @@ const nextConfig = {
   webpack: config => {
     config.resolve.modules = [path.resolve("./node_modules"), path.resolve("src")];
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
   }
 };
 
-module.exports = withFonts(withBundleAnalyzer(nextConfig));
+module.exports = withBundleAnalyzer(nextConfig);
